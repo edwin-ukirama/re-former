1ecfb97fd2e7a6308b1f7cc0143df83eb0dd20d3
Edwin Leonard Salim <edwin.salim@ukirama.com>
2022-09-26 16:13:19 +0700

    Add specific constraints to User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true
-  validates :email, presence: true
+  validates :username, presence: true, uniqueness: true, length: { in: 4..16 }
+  validates :email, presence: true, uniqueness: true
   validates :password, presence: true
 end
